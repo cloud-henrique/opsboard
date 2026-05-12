6fe65d82a594fa1d2de1bb0939dcc4c11509d79a
Cláudio Henrique <00claudio.henrique@gmail.com>
2026-05-12 00:37:34 -0300

    feat(ui): exibir dados do usuário em menu

--- frontend/src/app/core/layout/app-layout.ts
+++ frontend/src/app/core/layout/app-layout.ts
@@ -3,6 +3,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -26,6 +27,7 @@ interface NavItem {
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatMenuModule,
     MatSidenavModule,
     MatToolbarModule
   ],
